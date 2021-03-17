88403909d9d598cc39a866e6d1ff8a4557934bb8
Jonathan Kirkpatrick <jone.kirkpatrick@gmail.com>
2021-03-17 15:56:23 -0700

    Working on form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -5,24 +5,32 @@ const Form = (props) => {
     <section className="Contact">
       <div className="ContactInfo">
         <h4>Reach Out</h4>
-        <address>Jone.kirkpatrick@gmail.com</address>
-        <div className="GitHub">
-          <a href="mailto:Jone.kirkpatrick@gmail.com">
-            <img src="/email.png" alt="Email Link" />
-            Hit me up
-          </a>
-        </div>
-        <div className="GitHub">
-          <a href="https://github.com/jkirk001">
-            <img src="/GitHub-Mark-Light-32px.png" alt="GitHub Link" />
-            Check me out
-          </a>
+        <div className="ContactTargetList">
+          <address>Jone.kirkpatrick@gmail.com</address>
+          <div className="ContactInfoTarget">
+            <a href="mailto:Jone.kirkpatrick@gmail.com">
+              <img src="/email.png" alt="Email Link" />
+              <span>Hit me up</span>
+            </a>
+          </div>
+          <div className="ContactInfoTarget">
+            <a href="https://github.com/jkirk001">
+              <img src="/GitHub-Mark-Light-32px.png" alt="GitHub Link" />
+              <span>Check me out</span>
+            </a>
+          </div>
         </div>
       </div>
       <form className="ContactForm">
-        <label>Name</label>
-        <label>Email</label>
-        <label>Message</label>
+        <label htmlFor="name">Name</label>
+        <input id="name" placeholder="Put the name here" />
+        <label htmlFor="email">Email</label>
+        <input id="email" placeholder="...And the email here" />
+        <label htmlFor="message">Message</label>
+        <textarea
+          id="message"
+          placeholder="...And who would have thought the message goes here!"
+        ></textarea>
       </form>
     </section>
   );
