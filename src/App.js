f6087f50cae68f4c603c9b0c9d9d1d8e5f2d3d94
Jonathan Kirkpatrick <jone.kirkpatrick@gmail.com>
2021-03-17 13:08:12 -0700

    first 2 sections styled

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Nav from "./components/Nav/Nav";
+import Hello from "./components/Hello/Hello";
+import Tech from "./components/Tech/Tech";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Container">
+        <Nav />
+        <Hello />
+        <Tech />
+      </div>
     </div>
   );
 }
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,14 @@
+import "./Nav.css";
+
+const Nav = (props) => {
+  return (
+    <header className="Nav">
+      <h2 className="Logo">JEK</h2>
+      <a className="Hire" href="/">
+        Hire Me
+      </a>
+    </header>
+  );
+};
+
+export default Nav;
