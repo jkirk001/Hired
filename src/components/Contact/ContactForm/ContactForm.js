e9f54c37e9afc09a2b4e7c2977bd9e1cd6a8c047
Jonathan Kirkpatrick <jone.kirkpatrick@gmail.com>
2021-03-17 17:11:31 -0700

    Form is getting there

--- src/components/Contact/ContactForm/ContactForm.js
+++ src/components/Contact/ContactForm/ContactForm.js
@@ -3,15 +3,23 @@ import "./ContactForm.css";
 const ContactForm = (props) => {
   return (
     <form className="ContactForm">
-      <label htmlFor="name">Name</label>
-      <input id="name" placeholder="Put the name here" />
-      <label htmlFor="email">Email</label>
-      <input id="email" placeholder="...And the email here" />
-      <label htmlFor="message">Message</label>
-      <textarea
-        id="message"
-        placeholder="...And who would have thought the message goes here!"
-      ></textarea>
+      <h4>Quickie? </h4>
+      <div className="InputDiv">
+        <label htmlFor="name">Name</label>
+        <input id="name" placeholder="Put the name here" />
+      </div>
+      <div className="InputDiv">
+        <label htmlFor="email">Email</label>
+        <input id="email" placeholder="...And the email here" />
+      </div>
+      <div className="InputDiv">
+        <label htmlFor="message">Message</label>
+        <textarea
+          id="message"
+          placeholder="...And who would have thought the message goes here!"
+        ></textarea>
+      </div>
+      <button onClick={(e) => e.preventDefault()}>Submit</button>
     </form>
   );
 };
