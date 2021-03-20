53b7e90f6835eb48272f288030e960e6e9b56b4e
Jonathan Kirkpatrick <jone.kirkpatrick@gmail.com>
2021-03-19 19:51:03 -0700

    flipped it correctly

--- src/components/Tech/TechList/TechItem/TechItem.js
+++ src/components/Tech/TechList/TechItem/TechItem.js
@@ -5,13 +5,21 @@ import { useSpring, animated as a } from "react-spring";
 const TechItem = (props) => {
   const [flipped, set] = useState(false);
   const { transform, opacity } = useSpring({
-    opacity: flipped ? 1 : 0,
-    transform: `perspective(600px) rotateX(${flipped ? 180 : 0}deg)`,
+    opacity: flipped ? 0 : 1,
+    transform: `perspective(600px) rotateX(${flipped ? 0 : 180}deg)`,
     config: { mass: 5, tension: 400, friction: 30 },
   });
 
   return (
     <li className="TechListItem" onClick={() => set((state) => !state)}>
+      <a.div
+        className="c front"
+        style={{
+          opacity,
+          transform: transform.interpolate((t) => `${t} rotateX(180deg)`),
+          backgroundImage: `url(${props.tech.back})`,
+        }}
+      />
       <a.div
         className="c back"
         style={{ opacity: opacity.interpolate((o) => 1 - o), transform }}
@@ -37,14 +45,6 @@ const TechItem = (props) => {
           </article>
         </div>
       </a.div>
-      <a.div
-        className="c front"
-        style={{
-          opacity,
-          transform: transform.interpolate((t) => `${t} rotateX(180deg)`),
-          backgroundImage: `url(${props.tech.back})`,
-        }}
-      />
     </li>
   );
 };
